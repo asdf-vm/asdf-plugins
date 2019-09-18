8ad3aca5c83e2413c34453490dff43ada50d69da
OZAWA Sakuro <sakuro@2238.club>
2019-09-18 20:48:19 +0900

    Refactor the main loop

--- test.sh
+++ test.sh
@@ -1,16 +1,41 @@
 #!/usr/bin/env bash
 
+passing_heading='## Plugin List (Test Passing)'
+failing_heading='## Plugin List (Test Failing)'
+
+function extract_plugins() {
+  grep -o 'travis.*\.svg[^)]*' | sed "s~https?://~~; s:/: :; s/\.svg//; s/\?branch=/ /"
+}
+
+function passing_plugins() {
+  sed -n "/^${passing_heading}/,/^${failing_heading}/p" README.md | extract_plugins
+}
+
+function failing_plugins() {
+  sed -n "/^${failing_heading}/,$p" README.md | extract_plugins
+}
+
+function test_one_plugin() {
+  local host=$1
+  local repo=$2
+  local branch=$3
+  local travis_web_url="https://${host}/${repo}"
+  local url="https://api.${host}/repos/${repo}/branches/${branch}"
+
+  curl -sq -H 'Accept: application/vnd.travis-ci.2.1+json' ${url} | grep -q '"state":"passed"' && return 0
+
+  echo "Plugin build check failed:  ${travis_web_url}"
+  return 1
+}
+
 fails=0
 total=0
-while read -r -a plugin; do
+
+passing_plugins | while read -r host repo branch; do
   total=$((total+1))
-  travis_web_url="https://${plugin[0]}/${plugin[1]}"
-  url="https://api.${plugin[0]}/repos/${plugin[1]}/branches/${plugin[2]}"
-  if ! curl -sq -H 'Accept: application/vnd.travis-ci.2.1+json' ${url} | grep -q '"state":"passed"'; then
-    fails=$((fails+1))
-    echo "Plugin build check failed:  ${travis_web_url}"
-  fi
-done < <(sed -n '/^## Plugin List (Test Passing)/,/^## Plugin List (Test Failing)/p' README.md | grep -o 'travis.*\.svg[^)]*' | sed "s~https?://~~; s:/: :; s/\.svg//; s/\?branch=/ /")
+  test_one_plugin "$host" "$repo" "$branch" || fails=$((fails+1))
+  echo -n .
+done
 
 echo
 echo "Plugins available: ${total}"
