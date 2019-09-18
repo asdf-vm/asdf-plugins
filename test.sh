cf2810fbd45aa38383be71f06916df3d68147537
OZAWA Sakuro <sakuro@2238.club>
2019-09-18 21:35:58 +0900

    Test both passing/failing tests

--- test.sh
+++ test.sh
@@ -7,39 +7,59 @@ function extract_plugins() {
   grep -o 'travis.*\.svg[^)]*' | sed "s~https?://~~; s:/: :; s/\.svg//; s/\?branch=/ /"
 }
 
-function passing_plugins() {
-  sed -n "/^${passing_heading}/,/^${failing_heading}/p" README.md | extract_plugins
+function passing_plugin_section() {
+  sed -n "/^${passing_heading}/,/^${failing_heading}/p" README.md
 }
 
-function failing_plugins() {
-  sed -n "/^${failing_heading}/,$p" README.md | extract_plugins
+function failing_plugin_section() {
+  sed -n "/^${failing_heading}/,\$p" README.md
 }
 
 function test_one_plugin() {
   local host=$1
   local repo=$2
   local branch=$3
-  local travis_web_url="https://${host}/${repo}"
   local url="https://api.${host}/repos/${repo}/branches/${branch}"
 
-  curl -sq -H 'Accept: application/vnd.travis-ci.2.1+json' ${url} | grep -q '"state":"passed"' && return 0
-
-  echo "Plugin build check failed:  ${travis_web_url}"
-  return 1
+  curl -sq -H 'Accept: application/vnd.travis-ci.2.1+json' ${url} | grep -q '"state":"passed"'
 }
 
-fails=0
-total=0
+function test_plugin_list() {
+  local total=0
+  local fails=0
+  while read -r host repo branch; do
+    total=$((total+1))
+    test_one_plugin "$host" "$repo" "$branch" || {
+      fails=$((fails+1))
+      local travis_web_url="https://${host}/${repo}"
+      echo "Plugin build check failed:  ${travis_web_url}" >/dev/tty
+    }
+  done
+  echo "$total" "$fails"
+}
 
-passing_plugins | while read -r host repo branch; do
-  total=$((total+1))
-  test_one_plugin "$host" "$repo" "$branch" || fails=$((fails+1))
-  echo -n .
-done
+echo "Testing previously passing plugins"
+set -- $(passing_plugin_section | extract_plugins | test_plugin_list)
+total=$1
+pass_fail=$2
+pass_pass=$(($1-$2))
+
+echo "Testing previously failing plugins"
+set -- $(failing_plugin_section | extract_plugins | test_plugin_list)
+total=$((total+$1))
+fail_fail=$2
+fail_pass=$(($1-fail_fail))
+
+all_pass=$((pass_pass+fail_pass))
+all_fail=$((pass_fail+fail_fail))
 
 echo
 echo "Plugins available: ${total}"
-echo "Plugin build checks passed: $((total-fails))"
-echo "Plugin build checks failed: ${fails}"
+echo "Plugin build checks still pasing: $((pass_pass))"
+echo "Plugin build checks newly failed: $((pass_fail))"
+echo "Plugin build checks failure fixed: $((fail_pass))"
+echo "Plugin build checks still failing: $((fail_fail))"
+echo "Plugin build checks passed: ${all_pass} ($pass_pass + $fail_pass)"
+echo "Plugin build checks failed: ${all_fail} ($pass_fail + $fail_fail)"
 
-exit ${fails}
+exit ${pass_fail}
